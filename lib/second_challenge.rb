a9c2df4cec8d0820b80fb6984a298d05deb33e64
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 17:07:10 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,14 +2,9 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => "53",
       },
-      :matriarch => {
-        :name => "Lady Montague",
-        :age => "54",
-      },
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => [],
       },
     :capulet => {
