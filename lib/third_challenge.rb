306dbe60c6a23a4bf4fabd34f60490bdc457acbc
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 17:07:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,7 @@ def third_challenge
         name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -28,5 +28,5 @@ def third_challenge
    }
   }
 
-  
+
 end
