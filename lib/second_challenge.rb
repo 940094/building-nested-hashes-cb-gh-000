86a93ffb6cd32eda7abe34ecff2177bdd8b61abf
Justin Shaw <easyworkemail@gmail.com>
2019-04-30 17:06:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
         :name => "Lady Montague",
         :age => "54",
       },
-      :hero => {},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [],
       },
     :capulet => {
